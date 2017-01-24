a95c3b222f38930678c5750c354558c1c047403c
ZED <georlema@gmail.com>
2017-01-24 22:35:03 +0200

    refactor the script with new changes and new source of data

--- relic_plz.py
+++ relic_plz.py
@@ -1,89 +1,134 @@
-import dryscrape
-import facebook
+# import asyncio
+# import aiohttp
 import json
+import urllib.request
+import urllib.parse
+import datetime
 import os
+import facebook
+import dryscrape
 from time import sleep
 from PIL import Image
-from concurrent.futures import ThreadPoolExecutor
 
-# Selectors for country. ex. .gr for Greece, .cy for Cyprus
-# you can select one country or many but remember this will
-# get the best players between the selected countries.
-COUNTRY = ".gr,.cy"
+CONFIG = json.load(open("config.json"))
+
+COUNTRIES = ("gr", "cy")
 
 
-BASE_URL = "http://www.companyofheroes.com"
+def request(url, params, headers):
 
-SIDES = ("allies", "axis")
+    params = urllib.parse.urlencode(params)
+    url = "{}?{}".format(url, params)
+    request = urllib.request.Request(url, headers=headers)
 
-FACTIONS = ("soviet", "german", "aef", "wgerman", "british")
-
-MODES = ("1v1", "team-of-2", "team-of-3", "team-of-4")
-
-COOKIE = "agegate[passed]=yes; expires=Sat, 17-Nov-2018 18:00:00 GMT; domain=www.companyofheroes.com; path=/leaderboards"
-
-# For CLI-only Linux OS install xvfb and uncomment the following line.
-# dryscrape.start_xvfb()
+    response = urllib.request.urlopen(request)
+    return json.load(response)
 
 
-def css_country_selectors():
-    css = COUNTRY.split(sep=",")
+def get_leaderboards():
 
-    not_template = ":not({})"
-    css_not = not_template * len(css)
-    css_not = css_not.format(*css)
-    css_not = "a" + css_not
+    matchtypes = {}
 
-    css = ["a" + c for c in css]
-    css = ",".join(css)
-    return css, css_not
+    params = {'title': "coh2"}
+
+    response = request(CONFIG['leaderboards'], params, CONFIG['headers'])
+    # response = json.load(open("export.json"))
+
+    id1v1 = None
+    # get 1v1 matchtype id
+    for mt in response['matchTypes']:
+        if mt['name'] == '1V1':
+            id1v1 = mt['id']
+            break
+
+    # get leaderboard ids
+    for leaderboard in response['leaderboards']:
+        for m in leaderboard['leaderboardmap']:
+            if m['matchtype_id'] == id1v1:
+                matchtypes.update(
+                    {leaderboard['name']: leaderboard['id']})
+            elif m['statgroup_type'] in (2, 3, 4):
+                matchtypes.update(
+                    {leaderboard['name']: leaderboard['id']})
+
+    return matchtypes
 
 
-def search_leaderboards(prefs):
+def get_results(matchtype_id, sortBy=1, step=40, count=40):
+    # response = json.load(open("first.json"))
 
-    session = dryscrape.Session(base_url=BASE_URL)
-    session.set_attribute('auto_load_images', True)
-    session.set_cookie(COOKIE)
-
-    headers = [
-        'rank',
-        'level',
-        'players',
-        'streak',
-        'wins',
-        'losses',
-        'ratio',
-        'total',
-        'last_game']
-
-    page = 1
-    url = "leaderboards#global/{0}/{1}/by-rank?page=".format(*prefs)
-    results = []
-    selectors = css_country_selectors()
+    params = {
+        'leaderboard_id': matchtype_id,
+        'title': "coh2",
+        'platform': "PC_STEAM",
+        'sortBy': sortBy,
+        'start': 1,
+        'count': count
+    }
 
     while True:
-        session.visit(url + str(page))
-        sleep(3)
-        current_page = int(session.css('a.current')[0].text())
 
-        # if the current_page is the same with the previous then
-        # we have exhausted this ranking category and no one is found by this criteria.
-        if page > current_page:
-            return results
+        response = request(CONFIG['specific_leaderboard'], params, CONFIG['headers'])
 
-        print("{0}, {1}, page{2}".format(prefs[0], prefs[1], page))
-        matched = session.css(selectors[0])
-        for players in matched:
-            if players.parent().css(selectors[1]):
-                continue
-            data = players.parent().parent().children()
-            for d in data:
-                results.append(d.text().strip())
-            if "\n" in results[2]:
-                results[2] = results[2].split(sep="\n")
-            results = cast_to_int(results)
-            return dict(zip(headers, results))
-        page += 1
+        # if the leaderboardStats array is empty
+        # we have exhausted this category
+        if not response['leaderboardStats']:
+            return None
+
+        params['start'] += step
+
+        for stats in response['leaderboardStats']:
+            for group in response['statGroups']:
+                if stats['statGroup_id'] == group['id']:
+                    found = all(member['country'] in COUNTRIES for member in group['members'])
+                    if found:
+                        print("found for matchtype:", matchtype_id)
+                        results = dict(stats)
+                        results['total'] = results['wins'] + results['losses']
+                        results['ratio'] = "{:.0%}".format(results['wins'] / results['total'])
+                        results['players'] = [{'name': member['alias'], 'country': member['country']} for member in group['members']]
+                        results['last_game'] = get_last_game_datetime(results['lastMatchDate'])
+                        return results
+
+
+def get_last_game_datetime(date):
+
+    last_match_date = datetime.datetime.fromtimestamp(date)
+    now = datetime.datetime.now()
+    delta = now - last_match_date
+
+    if delta.days > 1:
+        return "{} days ago".format(delta.days)
+    elif delta.days == 1:
+        return "a day ago"
+    elif delta.seconds >= 0 and delta.seconds < 60:
+        return "less than a minute ago"
+    elif delta.seconds < 3600:
+        return "{} minutes ago".format(delta.seconds // 60)
+    elif delta.seconds >= 3600 and delta.seconds < 7200:
+        return "an hour ago"
+    else:
+        return "{} hours ago".format(delta.seconds // 3600)
+
+
+def normalize(data):
+
+    normalized = {
+        "1v1": {},
+        "team-of-2": {},
+        "team-of-3": {},
+        "team-of-4": {}
+    }
+
+    for matchtype, result in data:
+        if matchtype.startswith("1v1"):
+            normalized['1v1'].update({matchtype[3:]: result})
+        else:
+            num = matchtype[6]
+            key = "team-of-{}".format(num)
+            normalized[key].update({matchtype[7:]: result})
+
+    return normalized
 
 
 def get_fb_api(cfg):
@@ -97,48 +142,16 @@ def get_fb_api(cfg):
         return graph
 
 
-def cast_to_int(lst):
-    converted = []
-    for i in lst:
-        try:
-            converted.append(int(i))
-        except (ValueError, TypeError):
-            converted.append(i)
-    return converted
-
-
-def export(results, matches):
-
-    num_factions = len(FACTIONS)
-    onevone = results[:num_factions]
-    twovtwo = results[num_factions:num_factions + 2]
-    threevthree = results[num_factions + 2:num_factions + 4]
-    fourvfour = results[num_factions + 4:num_factions + 6]
-
-    json_data = {
-        MODES[0]: dict(zip(FACTIONS, onevone)),
-        MODES[1]: dict(zip(SIDES, twovtwo)),
-        MODES[2]: dict(zip(SIDES, threevthree)),
-        MODES[3]: dict(zip(SIDES, fourvfour))
-    }
-    with open("data.json", 'w') as json_file:
-        json.dump(json_data, json_file, indent=4)
-
-
 def main():
 
-    matches = []
-    matches.extend((MODES[0], faction) for faction in FACTIONS)
-    for i in range(1, len(MODES)):
-        matches.extend((MODES[i], side) for side in SIDES)
-
-    with ThreadPoolExecutor(max_workers=4) as executor:
-        results = executor.map(search_leaderboards, matches)
-        export(list(results), matches)
+    matchtypes = get_leaderboards()
+    results = [(matchtype, get_results(matchtype_id)) for matchtype, matchtype_id in matchtypes.items()]
+    results = normalize(results)
+    with open("newdata.json", 'w') as json_file:
+        json.dump(results, json_file, indent=4)
 
     path = os.path.dirname(os.path.abspath(__file__))
     session = dryscrape.Session()
-    session.set_attribute('auto_load_images', True)
     session.visit("file://{0}/{1}".format(path, "result.html"))
     sleep(3)
     session.render("results.png")
@@ -146,10 +159,9 @@ def main():
     stats_image = Image.open('results.png')
     stats_image = stats_image.crop((0, 0, 960, 1530)).save("results.png")
 
-    config = json.load(open("config.json"))
     fb_cfg = {
-        'page_id': config['FB_GROUP_ID'],
-        'access_token': config['FB_TOKEN']
+        'page_id': CONFIG['fb_group_id'],
+        'access_token': CONFIG['fb_token']
     }
 
     fb_api = get_fb_api(fb_cfg)
